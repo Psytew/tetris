9b9a1f687e911b90a90ffb8e812b3be4a2812379
Patrick <patrickwestervelt.us@gmail.com>
2019-08-23 16:12:19 -0400

    Basic Tetris Functionality

--- tetris.js
+++ tetris.js
@@ -3,11 +3,21 @@ const context = canvas.getContext('2d')
 
 context.scale(20,20)
 
-const matrix = [
-	[0, 0, 0],
-	[1, 1, 1],
-	[0, 1, 0]
-]
+function arenaSweep(){
+	let rowCounter = 1
+	outer: for (let y = arena.length - 1; y > 0; --y){
+		for (let x = 0; x < arena[y].length; ++x){
+			if (arena[y][x] === 0){
+				continue outer
+			}
+		}
+		const row = arena.splice(y, 1)[0].fill(0)
+		arena.unshift(row)
+		++y
+		player.score += rowCounter * 10
+		rowCounter *= 2
+	}
+}
 
 function collide(arena,player){
 	const [m, o] = [player.matrix, player.pos]
@@ -29,9 +39,56 @@ function createMatrix(w,h){
 	return matrix
 }
 
+function createPiece(type){
+	if (type == "T"){
+		return [
+			[0, 0, 0],
+			[1, 1, 1],
+			[0, 1, 0]
+		]
+	} else if (type == "O"){
+		return [
+			[2, 2],
+			[2, 2]
+		]
+	} else if (type == "L"){
+		return [
+			[0, 3, 0],
+			[0, 3, 0],
+			[0, 3, 3]
+		]
+	} else if (type == "J"){
+		return [
+			[0, 4, 0],
+			[0, 4, 0],
+			[4, 4, 0]
+		]
+	} else if (type == "I"){
+		return [
+			[0, 5, 0, 0],
+			[0, 5, 0, 0],
+			[0, 5, 0, 0],
+			[0, 5, 0, 0]
+		]
+	} else if (type == "S"){
+		return [
+			[0, 0, 0],
+			[0, 6, 6],
+			[6, 6, 0]
+		]
+	} else if (type == "Z"){
+		return [
+			[0, 0, 0],
+			[7, 7, 0],
+			[0, 7, 7]
+		]
+	}
+}
+
 function draw(){
 	context.fillStyle = '#000'
 	context.fillRect(0,0, canvas.width, canvas.height)
+	drawMatrix(arena, {x:0,y:0})
 	drawMatrix(player.matrix, player.pos)
 }
 
@@ -39,7 +96,7 @@ function drawMatrix(matrix, offset){
 	matrix.forEach((row, y) => {
 		row.forEach((value, x) => {
 			if (value != 0){
-				context.fillStyle = 'red'
+				context.fillStyle = colors[value]
 				context.fillRect(x + offset.x, 
 					y + offset.y, 
 					1, 1)
@@ -50,6 +107,13 @@ function drawMatrix(matrix, offset){
 
 function playerDrop(){
 	player.pos.y++
+	if (collide(arena,player)){
+		player.pos.y--
+		merge(arena,player)
+		playerReset()
+		arenaSweep()
+		updateScore()
+	}
 	dropCounter = 0
 }
 
@@ -63,8 +127,76 @@ function merge(arena,player){
 	})
 }
 
+function playerMove(dir){
+	player.pos.x += dir
+	if (collide(arena, player)) {
+		player.pos.x -= dir
+	}
+}
+
+function playerReset(){
+	const pieces = 'ILJOTSZ'
+	player.matrix = createPiece(pieces[pieces.length * Math.random() | 0])
+	player.pos.y = 0
+	player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0)
+	if (collide(arena,player)){
+		arena.forEach(row => row.fill(0))
+		player.score = 0
+		updateScore()
+	}
+}
+
+function playerRotate(dir){
+	const pos = player.pos.x
+	let offset = 1
+	rotate(player.matrix,dir)
+	while (collide(arena,player)){
+		player.pos.x += offset
+		offset = -(offset + (offset > 0 ? 1 : -1))
+		if (offset > player.matrix[0].length){
+			rotate(player.matrix, -dir)
+			player.pos.x = pos
+			return
+		}
+	}
+}
+
+function rotate(matrix, dir){
+	for (let y = 0; y < matrix.length; ++y){
+		for (let x = 0; x < y; ++x){
+			[
+				matrix[x][y],
+				matrix[y][x]
+			] = [
+				matrix[y][x],
+				matrix[x][y]
+			]
+		}
+	}
+	if (dir > 0){
+		matrix.forEach(row => row.reverse())
+	} else {
+		matrix.reverse()
+	}
+}
+
 let dropCounter = 0
 let dropInterval = 1000
+
+const colors = [
+  null,
+  'purple',
+  'yellow',
+  'orange',
+  'blue',
+  'aqua',
+  'green',
+  'red'
+];
+
+function updateScore(){
+	document.getElementById("score").innerText = player.score
+}
 
 let lastTime = 0
 function update(time = 0){
@@ -85,17 +217,24 @@ const arena = createMatrix(12,20)
 
 const player = {
 	pos: {x: 5, y: 5},
-	matrix: matrix
+	matrix: null,
+	score: 0
 }
 
 document.addEventListener('keydown', event => {
 	if (event.keyCode === 37){
-		player.pos.x--
+		playerMove(-1)
 	} else if (event.keyCode === 39){
-		player.pos.x++
+		playerMove(1)
 	} else if (event.keyCode === 40){
 		playerDrop()
+	} else if (event.keyCode === 81){
+		playerRotate(-1)
+	} else if (event.keyCode === 87){
+		playerRotate(1)
 	}
 })
 
+playerReset()
+updateScore()
 update()
